fafb706c0d7b8f0992257affd4800562c884483a
craig norford <craig.a.norford@gmail.com>
2022-08-28 17:49:27 +0100

    set creat button color

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -32,7 +32,7 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <NavLink className="nav-link" to="/create">
+              <NavLink className="btn btn-success" to="/create">
                 Create Record
               </NavLink>
             </li>
